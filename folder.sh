447871b4dd976d166abd8bc59f6ae3a4ddda5be5
Adnan Tariq <adnantariq2525@gmail.com>
2025-04-15 18:48:25 +0500

    Add directory structure with .gitkeep files

---
+++ folder.sh
@@ -0,0 +1,8 @@
+touch scripts/.gitkeep
+touch data/input/.gitkeep
+touch data/processed/.gitkeep
+touch results/taxonomy/.gitkeep
+touch results/functional/.gitkeep
+touch results/ml_models/.gitkeep
+touch images/.gitkeep
+
