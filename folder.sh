0148dee960140b5511374d09c534b724b042735a
Adnan Tariq <adnantariq2525@gmail.com>
2025-04-15 20:04:15 +0500

    Reomved extral file

--- folder.sh
+++
@@ -1,8 +0,0 @@
-touch scripts/.gitkeep
-touch data/input/.gitkeep
-touch data/processed/.gitkeep
-touch results/taxonomy/.gitkeep
-touch results/functional/.gitkeep
-touch results/ml_models/.gitkeep
-touch images/.gitkeep
-
